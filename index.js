4d3af405c03bd84d0888559bd4d87c05e80cb9c6
Jeremy Foster <jeremy.foster@live.com>
2017-12-18 14:07:30 -0800

    fix dependencies

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let request = require('request-promise');
+let cowsay = require('cowsay');
 
 async function getWeather(city) {
     city = encodeURI(city);
     let result = await request.get(`http://weathers.co/api.php?city=${city}`);
-    console.log(result);
+    result = JSON.parse(result);
+    console.log(cowsay.say({ text: result.data.temperature }));
 }
 
 getWeather('Seattle');
-getWeather('New York');
-getWeather('Orlando');
